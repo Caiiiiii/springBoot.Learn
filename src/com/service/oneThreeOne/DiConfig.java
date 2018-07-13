faec9bde0a80b3aee87ebdd237edbb179bab1563
Caiiiiiii <995017591@qq.com>
2018-07-13 21:54:18 +0800

    Initial commit

---
+++ src/com/service/oneThreeOne/DiConfig.java
@@ -0,0 +1,9 @@
+package com.service.oneThreeOne;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.service.oneThreeOne")
+public class DiConfig {
+}
