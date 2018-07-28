5d2d3b1ff02626f21e5f536f262cf155c169ee94
Caiiiiiii <995017591@qq.com>
2018-07-28 17:09:00 +0800

    chapter 3.3

---
+++ src/com/service/ThreeTwoTwo/Main.java
@@ -0,0 +1,18 @@
+package com.service.ThreeTwoTwo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(TaskExecutorConfig.class);
+
+        AsyncTaskService taskService = context.getBean(AsyncTaskService.class);
+
+        for (int i = 0 ;i<10;i++){
+            taskService.executeAsyncTask(i);
+            taskService.executeAsyncTaskPlus(i);
+        }
+        context.close();
+    }
+}
