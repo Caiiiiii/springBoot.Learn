ac516a4eb1be043415cc577ac44dce4caba271dc
Caiiiiiii <995017591@qq.com>
2018-07-28 16:04:37 +0800

    chapter 2.5.2

---
+++ src/com/service/TwoFiveTwo/EventConfig.java
@@ -0,0 +1,9 @@
+package com.service.TwoFiveTwo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.service.TwoFiveTwo")
+public class EventConfig {
+}
---
+++ src/com/service/TwoFourTwo/Main.java
@@ -0,0 +1,19 @@
+package com.service.TwoFourTwo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+
+        context.getEnvironment().setActiveProfiles("dev");    //将活动的profile 设置为 dev 或 prod
+        context.register(ProfileConfig.class);
+        context.refresh();
+
+        DemoBean demoBean = context.getBean(DemoBean.class);
+
+        System.out.println(demoBean.getContent());
+
+        context.close();
+    }
+}
